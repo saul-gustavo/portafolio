a4f2f40c7d8fe36a0009e9ccf136464c307b7df6
Saul Gustavo <saulgustavo237@gmail.com>
2024-11-08 13:16:51 -0600

    Iniciando repositorio Se agregaron los archivos del portafolio

---
+++ main.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector(".nav-menu");
+const navToggleBtn = document.querySelector(".menu-toggle");
+const navLinks = document.querySelectorAll(".nav-link");
+const moonIcono = document.getElementById('moon');
+const bodyDark = document.querySelector('.body')
+let esSol = false
+
+// navbar toggle function
+const navToggleFunc = function () {
+    nav.classList.toggle('active');
+    navToggleBtn.classList.toggle('active');
+}
+
+navToggleBtn.addEventListener('click', navToggleFunc);
+
+for (let i = 0; i < navLinks.length; i++) {
+
+    navLinks[i].addEventListener('click', navToggleFunc);
+
+}
+
+moonIcono.addEventListener('click', () => {
+
+})
